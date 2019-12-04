746d44f44414b9af0c50807b87d4f06a36c91c26
Siddharth Subramanian <sidsbrmnn@gmail.com>
2019-12-04 22:28:12 +0530

    Custom TextField Widget created

---
+++ lib/widgets/custom_textfield.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class CustomTextField extends StatelessWidget {
+  const CustomTextField({
+    @required this.labelText,
+    this.autofocus = false,
+    this.textCapitalization = TextCapitalization.none,
+  });
+
+  final String labelText;
+  final bool autofocus;
+  final TextCapitalization textCapitalization;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      decoration: InputDecoration(
+        labelText: labelText,
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(8.0),
+        ),
+      ),
+      autofocus: autofocus,
+      autocorrect: true,
+      textCapitalization: textCapitalization,
+    );
+  }
+}
