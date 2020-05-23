4c684e6d78953b579b7839036c92e9bb8d63f3f5
Varun S Athreya <varun.sathreya@gmail.com>
2020-05-24 01:02:05 +0530

    Page error handled and AndroidX support added.

--- lib/src/models.dart
+++ lib/src/models.dart
@@ -2,13 +2,13 @@ import 'package:flutter/material.dart';
 
 enum PageType { Tasks, Events }
 
-class Page {
+class AppPage {
   final String name;
   final PageType pageType;
   final Widget component;
   final Widget newItemPage;
 
-  Page({this.name, this.pageType, this.component, this.newItemPage});
+  AppPage({this.name, this.pageType, this.component, this.newItemPage});
 }
 
 class Event {
