f2ca7b7d478d5136723059df1f77e8bc26207f3d
Varun S Athreya <varun.sathreya@gmail.com>
2020-06-21 05:07:35 +0530

    function of addition, deletion and change in state of tasks done

---
+++ lib/src/tasks/Widgets/task_tile.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+class TaskTile extends StatelessWidget {
+  final bool isChecked;
+  final String taskTitle;
+  final Function checkboxCallback;
+  final Function longPressCallback;
+
+  TaskTile(
+      {this.isChecked,
+      this.taskTitle,
+      this.checkboxCallback,
+      this.longPressCallback,});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap:  checkboxCallback,
+      onLongPress: longPressCallback,
+      child: Container(
+        foregroundDecoration:
+            isChecked ? BoxDecoration(color: Color(0x60FDFDFD)) : null,
+        child: Padding(
+          padding: EdgeInsets.symmetric(vertical: 12.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+//              Checkbox(
+//                value: isChecked,
+//                activeColor: Theme.of(context).accentColor,
+//                onChanged: checkboxCallback,
+//              ),
+              Icon(
+                isChecked ?  Icons.radio_button_checked : Icons.radio_button_unchecked,
+                color: Theme.of(context).accentColor,
+                size: 24.0,
+              ),
+              SizedBox(
+                width: 16.0,
+              ),
+              Text(
+                taskTitle,
+                style: TextStyle(fontSize: 16.0),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+
+//    return ListTile(
+//      onLongPress: longPressCallback,
+//      title: Text(
+//        taskTitle,
+//        style: TextStyle(
+//          color: Colors.black,
+//          decoration: isChecked ? TextDecoration.lineThrough: null,
+//        ),
+//      ),
+//      leading: Checkbox(
+//        value: isChecked,
+//        activeColor: Theme.of(context).accentColor,
+//        onChanged: checkboxCallback,
+//      ),
+//    );
+  }
+}
---
+++ lib/src/tasks/screens/add_task_screen.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import '../../widgets/custom_button.dart';
+import 'package:provider/provider.dart';
+import '../../widgets/custom_textfield.dart';
+import '../models/tasks_data.dart';
+
+class AddTaskScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    String newTaskTitle;
+
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 24.0),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          Text(
+            'Add new task',
+            textAlign: TextAlign.center,
+            style: Theme.of(context).textTheme.title,
+          ),
+          SizedBox(
+            height: 24.0,
+          ),
+          CustomTextField(
+            labelText: 'Add new',
+            textCapitalization: TextCapitalization.sentences,
+            cursorColor: Theme.of(context).primaryColor,
+            autofocus: true,
+            onChanged: (newText) {
+              newTaskTitle = newText;
+            },
+          ),
+          SizedBox(
+            height: 16.0,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              CustomButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                text: 'Cancel',
+              ),
+              CustomButton(
+                onPressed: () {
+                  Provider.of<TasksData>(context, listen: false)
+                      .addTask(newTaskTitle);
+                  Navigator.pop(context);
+                },
+                text: 'Add',
+                color: Theme.of(context).accentColor,
+                textColor: Colors.white,
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
